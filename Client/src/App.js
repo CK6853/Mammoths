c20a41581d950e31009942e967966337dc52851e
CK6853 <caleb.klein@hotmail.com>
2024-10-11 17:17:35 +1000

    add some basic data passing

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
 
 function App() {
+
+  function search(formData) {
+    const requestOptions = {
+      method: "POST",
+      headers: { 'Content-Type': 'application/json'},
+      body: JSON.stringify({text: formData.get("test")})
+    }
+    fetch("http://phoenix.zapto.org:3002/", requestOptions)
+      .then(res => console.log(res.status))
+  }
+
   return (
     <div className="App">
       <h1>Mammoths - A Deep Dive</h1>
-      
+      <form action={search}>
+        <input name="test" />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
--- Server/routes/index.js
+++ Server/routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+// At index page, just display all data on the backend
 router.get('/', function(req, res, next) {
   //res.render('index', { title: 'Express' });
 
@@ -9,22 +9,39 @@ router.get('/', function(req, res, next) {
   res.json(DB.get("events"));
 });
 
+router.post("/", function(req, res, next) {
+  console.log(req.body)
+  res.status(200)
+  res.send()
+})
+
 router.get("/testing", async function (req, res, next) {
   DB = req.DB;
   let events = await DB.get("events")
   console.log(events)
   if (events === undefined) events = []
-  events.push("New event")
+  let newEvent = {
+    name: "Caleb",
+    email: "test@outlook.com",
+    phone: "0412345678",
+    date: "Will check format for this later"
+  }
+  events.push(newEvent)
   await DB.set("events", events)
   res.json("ok")
 })
 
 // API call to reset all data
-app.get('/reset', async function(req, res, next) {
+router.get('/reset', async function(req, res, next) {
+  // Nothing should go wrong, but if it does it will be handled by the default error handler
+  // Clear data
   await DB.deleteAll();
+  // Set an initial state
   DB.set("events", [])
+  // Server log
   console.log("Reset DB");
-  res.json("Reset");
+  // Tell the user
+  res.json("Reset");  
 })
 
 module.exports = router;
