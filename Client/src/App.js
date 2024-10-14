6c47d4a70f821bcac1d5f7a7acf5ff977e0d5dc6
CK6853 <caleb.klein@hotmail.com>
2024-10-14 15:31:02 +1000

    commenting pass

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { useState } from "react";
 
+// App body - just a heading and a custom component for the form
 function App() {
   return (
     <div className="App">
@@ -10,16 +11,22 @@ function App() {
   );
 }
 
-
+// Event form - no props
+// Holds all the inputs, state and API calls needed to register for an event
 function EventForm() {
 
+  // Set up state for date input - others can handle their own
   const [date, setDate] = useState("2025-05-24")
+  // Track current state of submission for display
   const [submissionStatus, setSubmissionStatus] = useState("Incomplete")
 
+  // Function to call on submission of form
+  // In normal use will only be called if form data is valid
   function submit(formData) {
     const requestOptions = {
       method: "POST",
       headers: { 'Content-Type': 'application/json'},
+      // Get data from inputs
       body: JSON.stringify({
         email: formData.get("email"),
         name: formData.get("name"),
@@ -27,19 +34,23 @@ function EventForm() {
         date: formData.get("date")
       })
     }
+    // Once we're sending the submission, set status to "pending" until we get a response
     setSubmissionStatus("Pending")
     fetch("http://phoenix.zapto.org:3002/", requestOptions)
       .then(res => {
         switch(res.status) {
+          // In case of an "ok" response, set status to "complete"
           case 200:
             setSubmissionStatus("Complete")
             break;
+          // Any other response - something has gone wrong
           default:
             setSubmissionStatus("Error")
         }
       })
   }
 
+  // Display a status message based on status state
   function submissionMessage() {
     switch (submissionStatus) {
       case "Incomplete":
@@ -47,7 +58,7 @@ function EventForm() {
       case "Pending":
         return "Sending registration request to server..."
       case "Complete":
-        return "Registration succcessful"
+        return "Registration successful"
       case "Error":
         return "Something went wrong. Please try again."
     }
@@ -56,6 +67,8 @@ function EventForm() {
   return (
     <div className="eventForm">
       <form action={submit}>
+        {/*Use a table to align everything nicely*/}
+        {/*Make a row for each input's label, input, and validation span*/}
         <table><tbody><tr>
             <td><label htmlFor="email">Email</label></td>
             <td><input name="email" id="email" type="email" placeholder="someone@example.com" autoComplete="email" required/>
