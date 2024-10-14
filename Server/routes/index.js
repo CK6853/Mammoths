3dcb8beaf4255aa4b42b2ba0516fbf578f8c13c6
CK6853 <caleb.klein@hotmail.com>
2024-10-14 17:25:32 +1000

    server data display

--- Server/routes/index.js
+++ Server/routes/index.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-// At index page, just display all data on the backend
-router.get('/', function(req, res, next) {
-  //res.render('index', { title: 'Express' });
-  DB = req.DB;
-  res.json(DB.get("events"));
+// If someone requests the index page, just display all data
+router.get('/', async function(req, res, next) {
+  let events = await req.DB.get("events")
+  res.render('index', { events: events });
 });
 
+// When the client sends a "post" request
 router.post("/", async function(req, res, next) {
   // Validation
   // Email must match standard format (apparently this Regex is equivalent to the default HTML email input pattern)
@@ -50,17 +50,17 @@ router.post("/", async function(req, res, next) {
   res.send()
 })
 
-// API call to reset all data
+// API route to reset all data
 router.get('/reset', async function(req, res, next) {
   // Nothing should go wrong, but if it does it will be handled by the default error handler
   // Clear data
-  await DB.deleteAll();
+  req.DB.deleteAll();
   // Set an initial state
-  DB.set("events", [])
+  req.DB.set("events", [])
   // Server log
   console.log("Reset DB");
   // Tell the user
-  res.json("Reset");  
+  res.json("Reset successful");  
 })
 
 module.exports = router;
